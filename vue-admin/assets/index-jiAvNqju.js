5eda853a0318a7529383932f0216d46d871f0318
chocho <849890769@qq.com>
2025-05-17 03:13:29 +0800

    feat: 更新

--- vue-admin/assets/index-jiAvNqju.js
+++
@@ -1,1 +0,0 @@
-import{_ as n,a4 as s,c,o as a,d as e,w as r}from"./index-GthB54wW.js";import{E as _}from"./el-alert-0XYjjzlO.js";const l={},i={style:{padding:"30px"}};function d(m,p){const t=s("RouterView"),o=_;return a(),c("div",i,[e(o,{closable:!1,title:"menu 1"},{default:r(()=>[e(t)]),_:1})])}const x=n(l,[["render",d]]);export{x as default};
