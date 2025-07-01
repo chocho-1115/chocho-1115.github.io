ec81284a3bdf7aad951ac5955ad0a31ede8dfca4
chocho <849890769@qq.com>
2025-07-01 22:28:54 +0800

    update

---
+++ vue-admin/assets/menu2-DRhHFWzt.js
@@ -0,0 +1,1 @@
+import{_ as t,c as o,o as c,d as n}from"./index-BV0bm1hi.js";import{E as s}from"./el-alert-BsX5a9Vs.js";const r={},a={style:{padding:"30px"}};function _(l,d){const e=s;return c(),o("div",a,[n(e,{closable:!1,title:"menu 2"})])}const i=t(r,[["render",_]]);export{i as default};
