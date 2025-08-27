e8fd99ed8cef9f59598ac8ba0b1ab710ad555c79
chocho <849890769@qq.com>
2025-08-27 18:24:41 +0800

    update

--- vue-admin/assets/index-BDxCYk66.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,aJ as c,c as n,o as a,d as e,w as r}from"./index-Bq9K9Tlp.js";import{E as _}from"./el-alert-C9_XC8Mw.js";const l={},i={style:{padding:"30px"}};function d(p,m){const t=c("RouterView"),o=_;return a(),n("div",i,[e(o,{closable:!1,title:"menu 1-2",type:"success"},{default:r(()=>[e(t)]),_:1})])}const x=s(l,[["render",d]]);export{x as default};
