88b407588416af154c34df6d216147b4d3d87764
chocho <849890769@qq.com>
2026-07-02 15:47:47 +0800

    更新

---
+++ vue-admin/assets/menu2-NK42k7Yw.js
@@ -0,0 +1,1 @@
+import"./style-_D-qWnI5.js";import{R as e,m as t,y as n}from"./runtime-core.esm-bundler-ErcdZhCV.js";import{t as r}from"./_plugin-vue_export-helper-BDNMzG2s.js";import{t as i}from"./style-tVwvaNfa.js";var a={},o={style:{padding:`30px`}};function s(r,a){let s=i;return e(),t(`div`,o,[n(s,{closable:!1,title:`menu 2`})])}var c=r(a,[[`render`,s]]);export{c as default};
