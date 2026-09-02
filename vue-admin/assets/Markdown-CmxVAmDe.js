556714bc22fcd694a7855b06c099d30044b95aa8
chocho-1115 <chocho-1115@users.noreply.github.com>
2026-09-02 10:08:44 +0000

    Deploy from chocho-1115/vue-admin@54b7005a3f128e67681ec8a5b78c026160a0de33 chocho-1115/vue-admin@54b7005a3f128e67681ec8a5b78c026160a0de33

---
+++ vue-admin/assets/Markdown-CmxVAmDe.js
@@ -0,0 +1,1 @@
+import{R as e,m as t}from"./runtime-core.esm-bundler-CpQGbZ0Q.js";import{s as n}from"./index-QukTml6f.js";var r=[`innerHTML`],i=n({__name:`Markdown`,props:{htmlContent:{type:String,default:``}},setup(n){return(i,a)=>(e(),t(`div`,{class:`content`,innerHTML:n.htmlContent},null,8,r))}},[[`__scopeId`,`data-v-4e84b3e8`]]);export{i as t};
